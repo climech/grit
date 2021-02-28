40aa11fbe5fa2a4851593ea84a4d6495f0534348
climech <climech@climech.org>
2021-02-28 14:57:48 +0100

    add multitree.sortNodesByName

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/climech/grit
 go 1.14
 
 require (
+	facette.io/natsort v0.0.0-20181210072756-2cd4dd1e2dcb // indirect
 	github.com/fatih/color v1.10.0
 	github.com/jawher/mow.cli v1.2.0
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible
--- multitree/sort.go
+++ multitree/sort.go
@@ -1,10 +1,22 @@
 package multitree
 
-import "sort"
+import (
+	"sort"
 
-// sortNodesByID sorts the nodes in-place by their ID field (ascending).
+	"facette.io/natsort"
+)
+
+// sortNodesByID sorts a slice of nodes in-place by Node.ID in ascending order.
 func sortNodesByID(nodes []*Node) {
 	sort.SliceStable(nodes, func(i, j int) bool {
 		return nodes[i].ID < nodes[j].ID
+	})
+}
+
+// sortNodesByName uses natural sort to sort a slice of nodes in-place by
+// Node.Name in ascending order.
+func sortNodesByName(nodes []*Node) {
+	sort.SliceStable(nodes, func(i, j int) bool {
+		return natsort.Compare(nodes[i].Name, nodes[j].Name)
 	})
 }
