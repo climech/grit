c5a72bc91515a86576b0f5779022b546505e0d68
climech <climech@climech.org>
2021-03-01 01:38:49 +0100

    switch to a custom naturalsort module

--- multitree/sort.go
+++ multitree/sort.go
@@ -3,7 +3,7 @@ package multitree
 import (
 	"sort"
 
-	"facette.io/natsort"
+	"github.com/climech/naturalsort"
 )
 
 // SortNodesByID sorts a slice of nodes in-place by Node.ID in ascending order.
@@ -17,6 +17,6 @@ func SortNodesByID(nodes []*Node) {
 // Node.Name in ascending order.
 func SortNodesByName(nodes []*Node) {
 	sort.SliceStable(nodes, func(i, j int) bool {
-		return natsort.Compare(nodes[i].Name, nodes[j].Name)
+		return naturalsort.Compare(nodes[i].Name, nodes[j].Name)
 	})
 }
